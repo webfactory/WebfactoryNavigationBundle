a493b070560dbee80b76a098565dab9fb89b4d0e
Per Bernhardt <plb@webfactory.de>
2011-12-20 13:50:07 -0100

    Bugfixes

--- DependencyInjection/Compiler/UrlGeneratorPass.php
+++ DependencyInjection/Compiler/UrlGeneratorPass.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 class UrlGeneratorPass implements CompilerPassInterface {
 
     public function process(ContainerBuilder $container) {
-        $definition = $container->getDefinition('webfactory.navigation.twig.extension');
+        $definition = $container->getDefinition('webfactory.navigation.twig_extension');
 
         foreach ($container->findTaggedServiceIds('webfactory.navigation.url_generator') as $id => $tags) {
             $definition->addMethodCall('addUrlGenerator', array(new Reference($id)));
