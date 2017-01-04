b4b6eef272a16a520b81098a1ee6b2cf1b565099
Matthias Pigulla <mp@webfactory.de>
2017-01-04 12:45:22 +0100

    Add a Node::getPath() method, tweak docblocks and CS

--- Tree/Node.php
+++ Tree/Node.php
@@ -13,20 +13,27 @@ class Node implements \ArrayAccess
     /**
      * @var Node[]
      */
-    protected $children = array();
+    protected $children = [];
 
     /**
      * @var Node|null
      */
     protected $parent = null;
 
-    protected $data = array('visible' => false, 'breadcrumbsVisible' => true, 'url' => false, 'caption' => '');
+    protected $data = ['visible' => false, 'breadcrumbsVisible' => true, 'url' => false, 'caption' => ''];
 
     /**
      * @var Tree
      */
     protected $tree;
 
+    /**
+     * Adds a new child node.
+     *
+     * @param Node|null $n The node to add as a child. If null, a new Node will be created (and returned).
+     *
+     * @return Node Returns the given or newly created node, for use in fluent notations.
+     */
     public function addChild(Node $n = null)
     {
         if ($n === null) {
@@ -40,11 +47,6 @@ class Node implements \ArrayAccess
         return $n;
     }
 
-    protected function setParent(Node $p)
-    {
-        $this->parent = $p;
-    }
-
     public function setTree(Tree $t)
     {
         $this->tree = $t;
@@ -55,6 +57,13 @@ class Node implements \ArrayAccess
         return $this->tree;
     }
 
+    /**
+     * Adds this node to the Tree's node index, so that it can later be retrieved by calling \Webfactory\Bundle\NavigationBundle\Tree\Tree::find.
+     *
+     * @param array $requirements An array of key-value-pairs used for indexing. If a later call to \Webfactory\Bundle\NavigationBundle\Tree\Tree::find passes at least these key-value-pairs, the node will be found.
+     *
+     * @return $this The node itself, for use in fluent notations.
+     */
     public function index(array $requirements)
     {
         $this->tree->addFindIndex($this, $requirements);
@@ -68,6 +77,14 @@ class Node implements \ArrayAccess
         return $this->index($requirements);
     }
 
+    /**
+     * Sets a value on this node.
+     *
+     * @param string $name The value's name.
+     * @param mixed $value The value's value.
+     *
+     * @return $this The node itself, for use in fluent notations.
+     */
     public function set($name, $value)
     {
         $this->data[$name] = $value;
@@ -76,8 +93,11 @@ class Node implements \ArrayAccess
     }
 
     /**
-     * @param string $name
-     * @return mixed|null
+     * Gets a value stored in this node.
+     *
+     * @param string $name Name of the value to get.
+     *
+     * @return mixed|null The value, or null if the $name is unknown.
      */
     public function get($name)
     {
@@ -94,23 +114,47 @@ class Node implements \ArrayAccess
         return $this->data;
     }
 
+    /**
+     * @return null|Node Returns the parent node.
+     */
     public function getParent()
     {
         return $this->parent;
     }
 
+    /**
+     * @return Node[] The array of all Nodes from the root towards this node.
+     */
+    public function getPath()
+    {
+        if ($this->parent === null) {
+            return [$this];
+        } else {
+            $p = $this->parent->getPath();
+            $p[] = $this;
+
+            return $p;
+        }
+    }
+
+    /**
+     * @return Node[] Returns all child nodes.
+     */
     public function getChildren()
     {
         return $this->children;
     }
 
+    /**
+     * @return bool Whether the node has child nodes or not.
+     */
     public function hasChildren()
     {
         return (bool)$this->children;
     }
 
     /**
-     * @return bool
+     * @return bool Whether the node has visible child nodes or not.
      */
     public function hasVisibleChildren()
     {
@@ -123,16 +167,29 @@ class Node implements \ArrayAccess
         return false;
     }
 
+    /**
+     * @param Node $ancestor
+     *
+     * @return bool True if the given node is an ancestor of the current node.
+     */
     public function hasAncestor(Node $ancestor)
     {
         return $this->parent && (($this->parent === $ancestor) || $this->parent->hasAncestor($ancestor));
     }
 
+    /**
+     * @param Node $descendant
+     *
+     * @return bool True if $descendant is a descendant of the current node.
+     */
     public function hasDescendant(Node $descendant)
     {
         return $descendant->hasAncestor($this);
     }
 
+    /**
+     * @return int Returns the level of this node, with "0" being the root level.
+     */
     public function getLevel()
     {
         if ($this->parent) {
@@ -142,6 +199,11 @@ class Node implements \ArrayAccess
         }
     }
 
+    /**
+     * Sets this node as the currently "active" (in terms of navigation state) node in the Tree.
+     *
+     * @return $this The node itself, for use in fluent notations.
+     */
     public function setActive()
     {
         $this->tree->setActiveNode($this);
@@ -149,6 +211,20 @@ class Node implements \ArrayAccess
         return $this;
     }
 
+    /**
+     * Sets this node as the "closest to active" node in the Tree.
+     *
+     * Imagine, for example, a "gallery" page that shows some featured items from your shop
+     * and also provides a form to filter or search for articles. When the user submits this
+     * form, you show her a "results" page, but clearly this is not the same as your "gallery".
+     *
+     * So, you could use setActivePath() on the "gallery" node, so it remains highlighted in
+     * the navigation. But, as it is not really the currently active page, it will only be
+     * in "active path" state and, among other things, still provide a clickable link to return
+     * to the gallery page.
+     *
+     * @return $this The node itself, for use in fluent notations.
+     */
     public function setActivePath()
     {
         $this->tree->setActivePath($this);
@@ -156,11 +232,17 @@ class Node implements \ArrayAccess
         return $this;
     }
 
+    /**
+     * @return bool Whether this is the currently active node in the tree.
+     */
     public function isActiveNode()
     {
         return $this->tree->getActiveNode() === $this;
     }
 
+    /**
+     * @return bool Whether this node lies on the path from the Tree root towards the active node.
+     */
     public function isActivePath()
     {
         if (!($ap = $this->tree->getActivePath())) {
@@ -190,6 +272,11 @@ class Node implements \ArrayAccess
         unset($this->data[$offset]);
 
         return $this;
+    }
+
+    protected function setParent(Node $p)
+    {
+        $this->parent = $p;
     }
 
 }
--- Tree/Tree.php
+++ Tree/Tree.php
@@ -13,7 +13,24 @@ class Tree
     protected $roots = array();
     protected $identities = array();
     protected $finder;
-    protected $activeNode = null, $activePath = null;
+
+    /**
+     * The currently active node in this tree.
+     *
+     * @var Node|null
+     */
+    protected $activeNode = null;
+
+    /**
+     * The "closest to active" node in this tree.
+     *
+     * Used whenever the "real" active node does not exist in the tree, for example
+     * because it results from user input and we cannot enumerate all the possible
+     * nodes when building the tree.
+     *
+     * @var Node|null
+     */
+    protected $activePath = null;
 
     public function __construct()
     {
@@ -46,22 +63,43 @@ class Tree
         return $this->finder->lookup($provisions);
     }
 
+    /**
+     * Sets the active Node.
+     *
+     * @param Node $n
+     */
     public function setActiveNode(Node $n)
     {
         $this->activeNode = $this->activePath = $n;
     }
 
+    /**
+     * Sets a node as the "closest to active" node in the tree, but making this particular
+     * node itself *not* active.
+     *
+     * @param Node $n
+     */
     public function setActivePath(Node $n)
     {
         $this->activeNode = null;
         $this->activePath = $n;
     }
 
+    /**
+     * @return Node|null Returns the currently active node, if available.
+     */
     public function getActiveNode()
     {
         return $this->activeNode;
     }
 
+    /**
+     * Returns the node that is currently active or comes closest to the actually active state. Use
+     * \Webfactory\Bundle\NavigationBundle\Tree\Node::isActiveNode to query whether the node is indeed active
+     * or not.
+     *
+     * @return Node|null
+     */
     public function getActivePath()
     {
         return $this->activePath;
