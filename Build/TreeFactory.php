fdaa02f96b911681a75e0dc1e616c73cfbb4b9f3
Matthias Pigulla <mp@webfactory.de>
2021-05-21 10:39:27 +0000

    Fix a deprecation notice issued by symfony/event-dispatcher ^4.3

--- Build/TreeFactory.php
+++ Build/TreeFactory.php
@@ -114,8 +114,10 @@ class TreeFactory implements ServiceSubscriberInterface
             }
 
             if ($this->eventDispatcher) {
-                $this->eventDispatcher->dispatch('webfactory_navigation.tree_initialized',
-                    new TreeInitializedEvent($this->_tree));
+                $this->eventDispatcher->dispatch(
+                    new TreeInitializedEvent($this->_tree),
+                    'webfactory_navigation.tree_initialized'
+                );
             }
         }
 
